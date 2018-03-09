61c5b3bb2257cc13afeaa6bd68e207dd5574af80
ronok <fuad.ronok@gmail.com>
2018-03-10 03:06:03 +0600

    Initial Commit

---
+++ resources/assets/js/components/TestComponent.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div>
+        <h1>This is vue Test Component</h1>
+
+    </div>
+
+</template>
+
+
+
+<script>
+    export default{
+        data(){
+            return{
+                articles:[],
+                edit:''
+            }
+        }
+    }
+
+</script>
